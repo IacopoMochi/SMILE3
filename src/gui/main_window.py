9195ed7372689e73e6212c790885590df288f28d
Emilia <emiliaburdak@gmail.com>
2024-05-29 13:54:33 +0200

    refactor: isolate add_new_row function from load_lines_image and change variable name from cnt to image_id

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -14,19 +14,19 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi("view/SMILE3.ui", self)
-        self.line_image_list = LineImageList(-1,'Average','Empty', 'Lines')
+        self.line_image_list = LineImageList(-1, 'Average', 'Empty', 'Lines')
         self.pushButton_ImageFolder.pressed.connect(self.load_lines_image)
         self.process_lines_button.pressed.connect(self.process_line_images)
         self.linesTable.cellClicked.connect(self.navigateLinesTable)
         self.metric_selection.clicked.connect(self.display_lines_data)
         self.export_data.clicked.connect(self.export_lines_data)
 
-# saves data to excel file
+    # saves data to excel file
     def export_lines_data(self):
         wb = Workbook()
         wb.save('test.xlsx')
 
-# display image data on GUI chart, plot -> smaller functions
+    # display image data on GUI chart, plot -> smaller functions
     def display_lines_data(self, Image):
 
         # Display lines image on the axis in the parameters tab
@@ -34,10 +34,9 @@ class MainWindow(QtWidgets.QMainWindow):
         plot_item_parameters.clear()
         if not (self.line_image_list.current_image == -2):
             image_item_parameters = pg.ImageItem(np.array(Image.image))
-            ROI = pg.RectROI((0,0),(100,100))
+            ROI = pg.RectROI((0, 0), (100, 100))
             self.line_image_view_parameters.addItem(image_item_parameters)
             self.line_image_view_parameters.addItem(ROI)
-
 
         # Display lines image on the top axis of the lines tab
         # Clean display
@@ -62,12 +61,12 @@ class MainWindow(QtWidgets.QMainWindow):
                     self.line_image_view.addItem(leading_edge_plot)
             if not (Image.trailing_edges is None):
                 for edge in Image.trailing_edges:
-                    trailing_edge_plot = pg.PlotDataItem(edge, np.arange(0,profiles_length), pen=edge_pen)
+                    trailing_edge_plot = pg.PlotDataItem(edge, np.arange(0, profiles_length), pen=edge_pen)
                     self.line_image_view.addItem(trailing_edge_plot)
             # Display additional stuff (errors, defects, etc)
 
         else:
-            if not(self.line_image_list.current_image == -2):
+            if not (self.line_image_list.current_image == -2):
                 image_item = pg.ImageItem(np.array(Image.image))
                 self.line_image_view.addItem(image_item)
 
@@ -94,11 +93,15 @@ class MainWindow(QtWidgets.QMainWindow):
 
             if self.histogram.isChecked() and not self.line_image_list.current_image == -2:
                 histogram_plot = pg.PlotDataItem(np.linspace(0, 255, 256), Image.intensity_histogram, pen=histogram_pen)
-                histogram_plot_low = pg.PlotDataItem(np.linspace(0, 255, 256), Image.intensity_histogram_low, pen=histogram_curves_pen)
-                histogram_plot_medium = pg.PlotDataItem(np.linspace(0, 255, 256), Image.intensity_histogram_medium, pen=histogram_curves_pen)
-                histogram_plot_high = pg.PlotDataItem(np.linspace(0, 255, 256), Image.intensity_histogram_high, pen=histogram_curves_pen)
-                histogram_plot_fit = pg.PlotDataItem(np.linspace(0, 255, 256), Image.intensity_histogram_high+Image.intensity_histogram_low+Image.intensity_histogram_medium,
-                                                      pen=histogram_fit_pen)
+                histogram_plot_low = pg.PlotDataItem(np.linspace(0, 255, 256), Image.intensity_histogram_low,
+                                                     pen=histogram_curves_pen)
+                histogram_plot_medium = pg.PlotDataItem(np.linspace(0, 255, 256), Image.intensity_histogram_medium,
+                                                        pen=histogram_curves_pen)
+                histogram_plot_high = pg.PlotDataItem(np.linspace(0, 255, 256), Image.intensity_histogram_high,
+                                                      pen=histogram_curves_pen)
+                histogram_plot_fit = pg.PlotDataItem(np.linspace(0, 255, 256),
+                                                     Image.intensity_histogram_high + Image.intensity_histogram_low + Image.intensity_histogram_medium,
+                                                     pen=histogram_fit_pen)
                 self.metric_plot.clear()
                 self.metric_plot.addItem(histogram_plot)
                 self.metric_plot.addItem(histogram_plot_low)
@@ -110,10 +113,13 @@ class MainWindow(QtWidgets.QMainWindow):
             elif self.lineWidthPSD.isChecked():
 
                 LW_PSD_plot = pg.PlotDataItem(Image.frequency, Image.LWR_PSD[0:len(Image.frequency)], pen=PSD_pen)
-                LW_PSD_fit_plot = pg.PlotDataItem(Image.frequency, Image.LWR_PSD_fit[0:len(Image.frequency)], pen=PSD_fit_pen)
-                LW_PSD_unbiased_plot = pg.PlotDataItem(Image.frequency, Image.LWR_PSD_unbiased[0:len(Image.frequency)], pen=PSD_unbiased_pen)
-                LW_PSD_fit_unbiased_plot = pg.PlotDataItem(Image.frequency, Image.LWR_PSD_fit_unbiased[0:len(Image.frequency)],
-                                                  pen=PSD_fit_unbiased_pen)
+                LW_PSD_fit_plot = pg.PlotDataItem(Image.frequency, Image.LWR_PSD_fit[0:len(Image.frequency)],
+                                                  pen=PSD_fit_pen)
+                LW_PSD_unbiased_plot = pg.PlotDataItem(Image.frequency, Image.LWR_PSD_unbiased[0:len(Image.frequency)],
+                                                       pen=PSD_unbiased_pen)
+                LW_PSD_fit_unbiased_plot = pg.PlotDataItem(Image.frequency,
+                                                           Image.LWR_PSD_fit_unbiased[0:len(Image.frequency)],
+                                                           pen=PSD_fit_unbiased_pen)
                 self.metric_plot.clear()
                 if self.metric_original_data.isChecked():
                     self.metric_plot.addItem(LW_PSD_plot)
@@ -131,12 +137,12 @@ class MainWindow(QtWidgets.QMainWindow):
             elif self.LineEdgePSD.isChecked():
                 LER_PSD_plot = pg.PlotDataItem(Image.frequency, Image.LER_PSD[0:len(Image.frequency)], pen=PSD_pen)
                 LER_PSD_fit_plot = pg.PlotDataItem(Image.frequency, Image.LER_PSD_fit[0:len(Image.frequency)],
-                                                  pen=PSD_fit_pen)
+                                                   pen=PSD_fit_pen)
                 LER_PSD_unbiased_plot = pg.PlotDataItem(Image.frequency, Image.LER_PSD_unbiased[0:len(Image.frequency)],
-                                                       pen=PSD_unbiased_pen)
+                                                        pen=PSD_unbiased_pen)
                 LER_PSD_fit_unbiased_plot = pg.PlotDataItem(Image.frequency,
-                                                           Image.LER_PSD_fit_unbiased[0:len(Image.frequency)],
-                                                           pen=PSD_fit_unbiased_pen)
+                                                            Image.LER_PSD_fit_unbiased[0:len(Image.frequency)],
+                                                            pen=PSD_fit_unbiased_pen)
                 self.metric_plot.clear()
                 if self.metric_original_data.isChecked():
                     self.metric_plot.addItem(LER_PSD_plot)
@@ -191,14 +197,18 @@ class MainWindow(QtWidgets.QMainWindow):
 
                 self.metric_plot.setAutoVisible(y=True)
             elif self.TrailingEdgePSD.isChecked():
-                trailing_LER_PSD_plot = pg.PlotDataItem(Image.frequency, Image.LER_Trailing_PSD[0:len(Image.frequency)], pen=PSD_pen)
-                trailing_LER_PSD_fit_plot = pg.PlotDataItem(Image.frequency, Image.LER_Trailing_PSD_fit[0:len(Image.frequency)],
-                                                  pen=PSD_fit_pen)
-                trailing_LER_PSD_unbiased_plot = pg.PlotDataItem(Image.frequency, Image.LER_Trailing_PSD_unbiased[0:len(Image.frequency)],
-                                                       pen=PSD_unbiased_pen)
+                trailing_LER_PSD_plot = pg.PlotDataItem(Image.frequency, Image.LER_Trailing_PSD[0:len(Image.frequency)],
+                                                        pen=PSD_pen)
+                trailing_LER_PSD_fit_plot = pg.PlotDataItem(Image.frequency,
+                                                            Image.LER_Trailing_PSD_fit[0:len(Image.frequency)],
+                                                            pen=PSD_fit_pen)
+                trailing_LER_PSD_unbiased_plot = pg.PlotDataItem(Image.frequency, Image.LER_Trailing_PSD_unbiased[
+                                                                                  0:len(Image.frequency)],
+                                                                 pen=PSD_unbiased_pen)
                 trailing_LER_PSD_fit_unbiased_plot = pg.PlotDataItem(Image.frequency,
-                                                           Image.LER_Trailing_PSD_fit_unbiased[0:len(Image.frequency)],
-                                                           pen=PSD_fit_unbiased_pen)
+                                                                     Image.LER_Trailing_PSD_fit_unbiased[
+                                                                     0:len(Image.frequency)],
+                                                                     pen=PSD_fit_unbiased_pen)
                 self.metric_plot.clear()
                 if self.metric_original_data.isChecked():
                     self.metric_plot.addItem(trailing_LER_PSD_plot)
@@ -216,16 +226,16 @@ class MainWindow(QtWidgets.QMainWindow):
         # pci = pg.PlotCurveItem(x=[1, 50, 100, 150, 200], y=[1, 50, 100, 150, 200])
         # image_view.addItem(pci)
 
-# process each image from LineImageList (smile_image_list_class) and updates GUI with metrics
-# check how meny images has been selected
-# call gather_parameters() along with pre_processing(), find_edges(), calculate_metrics() (smile_lines_image_class) for each image
-# update the parameters in table
-# use gather_edges() from mile_image_list_class()
+    # process each image from LineImageList (smile_image_list_class) and updates GUI with metrics
+    # check how meny images has been selected
+    # call gather_parameters() along with pre_processing(), find_edges(), calculate_metrics() (smile_lines_image_class) for each image
+    # update the parameters in table
+    # use gather_edges() from mile_image_list_class()
     def process_line_images(self):
         # Count how many images have been selected for processing
         number_of_selected_images = 0
         for lines_image in self.line_image_list.lineImages:
-            if self.linesTable.item(lines_image.id,0).checkState() == Qt.CheckState.Checked:
+            if self.linesTable.item(lines_image.id, 0).checkState() == Qt.CheckState.Checked:
                 number_of_selected_images += 1
         number_of_processed_images = 0
         self.image_progressBar.setMinimum(0)
@@ -233,8 +243,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.image_progressBar.setValue(0)
 
         for lines_image in self.line_image_list.lineImages:
-            self.status_label.setText("Processing " + str(number_of_processed_images+1) + " of " + str(number_of_selected_images))
-            if self.linesTable.item(lines_image.id,0).checkState() == Qt.CheckState.Checked:
+            self.status_label.setText(
+                "Processing " + str(number_of_processed_images + 1) + " of " + str(number_of_selected_images))
+            if self.linesTable.item(lines_image.id, 0).checkState() == Qt.CheckState.Checked:
                 self.gather_parameters(lines_image)
                 self.line_image_list.current_image = lines_image.id
                 lines_image.pre_processing()
@@ -266,8 +277,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.line_image_list.calculate_metrics()
         self.status_label.setText("Ready")
 
-# helper function to prepare image parameters needed by SmileLinesImage (smile_lines_image_class)
-# parameters collected from UI elements
+    # helper function to prepare image parameters needed by SmileLinesImage (smile_lines_image_class)
+    # parameters collected from UI elements
     def gather_parameters(self, Image):
 
         if self.Polynomial.isChecked():
@@ -303,7 +314,7 @@ class MainWindow(QtWidgets.QMainWindow):
                       }
         Image.parameters = parameters
 
-# helper function, handles the navigation in display selected line image data when a cell in the lines table is clicked
+    # helper function, handles the navigation in display selected line image data when a cell in the lines table is clicked
     def navigateLinesTable(self, nrow, ncol):
         self.line_image_list.current_image = nrow
         if nrow < len(self.line_image_list.lineImages):
@@ -312,17 +323,15 @@ class MainWindow(QtWidgets.QMainWindow):
             self.line_image_list.current_image = -2
             self.display_lines_data(self.line_image_list)
 
-
-# open a dialog to select an image folder, loads image
-# call function gather parameters (main_controller)
-# append image object (in loop) to LineImageList (smile_image_list_class)
+    # open a dialog to select an image folder, loads image
+    # call function gather parameters (main_controller)
+    # append image object (in loop) to LineImageList (smile_image_list_class)
     def load_lines_image(self):
         # Initialize image list
         self.line_image_list = LineImageList('-1', 'imageList', 'Empty', 'Empty')
 
-        # Open folder dialog
         file_names = folder_loader()
-        cnt = -1
+        image_id = -1
         for root, dirs, files in os.walk(file_names[0], topdown=True):
             for name in files:
                 if (
@@ -330,33 +339,36 @@ class MainWindow(QtWidgets.QMainWindow):
                         | name.endswith(".jpg")
                         | name.endswith(".png")
                 ):
-                    cnt += 1
-                    # self.linesTable.setRowCount (cnt+2)
-                    image_object = SmileLinesImage(cnt, name, root, "lines")
-                    image_object.load_image()
-                    self.gather_parameters(image_object)
-                    self.line_image_list.lineImages.append(
-                        image_object
-                    )
-                    item_name = QtWidgets.QTableWidgetItem(image_object.file_name)
+                    image_id += 1
+                    self.add_new_row(image_id, name, root)
+                else:
+                    raise ValueError('Invalid file extension')
 
-                    item_selected = QtWidgets.QTableWidgetItem()
-                    item_selected.setFlags(
-                        Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEnabled
-                    )
-                    item_selected.setCheckState(Qt.CheckState.Checked)
+    def add_new_row(self, image_id, name, root):
+        image_object = SmileLinesImage(image_id, name, root, "lines")
+        image_object.load_image()
+        self.gather_parameters(image_object)
+        self.line_image_list.lineImages.append(
+            image_object
+        )
+        item_name = QtWidgets.QTableWidgetItem(image_object.file_name)
+        item_selected = QtWidgets.QTableWidgetItem()
+        item_selected.setFlags(
+            Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEnabled
+        )
+        item_selected.setCheckState(Qt.CheckState.Checked)
+        item_processed = QtWidgets.QTableWidgetItem()
+        item_processed.setFlags(
+            Qt.ItemFlag.ItemIsEnabled
+        )
+        item_processed.setCheckState(Qt.CheckState.Unchecked)
 
-                    item_processed = QtWidgets.QTableWidgetItem()
-                    item_processed.setFlags(
-                        Qt.ItemFlag.ItemIsEnabled
-                    )
-                    item_processed.setCheckState(Qt.CheckState.Unchecked)
-
-                    self.linesTable.setRowCount(cnt + 2)
-                    self.linesTable.setItem(cnt, 0, item_selected)
-                    self.linesTable.setItem(cnt, 1, item_processed)
-                    self.linesTable.setItem(cnt, 2, item_name)
-                    self.display_lines_data(image_object)
+        # self.linesTable.setRowCount (image_id+2)
+        self.linesTable.setRowCount(image_id + 2)
+        self.linesTable.setItem(image_id, 0, item_selected)
+        self.linesTable.setItem(image_id, 1, item_processed)
+        self.linesTable.setItem(image_id, 2, item_name)
+        self.display_lines_data(image_object)
 
 
 def folder_loader():
@@ -367,8 +379,3 @@ def folder_loader():
         return file_names
     else:
         raise ValueError('Cannot open this folder')
-
-
-
-
-
