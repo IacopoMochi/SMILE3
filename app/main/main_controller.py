3930aeb79c4b9d8b80fb26ad4ee729a56cd6fe23
Emilia <emiliaburdak@gmail.com>
2024-06-04 09:29:15 +0200

    refactor: loading image and table controller use the same ui file

--- app/main/main_controller.py
+++ app/main/main_controller.py
@@ -8,22 +8,21 @@ from openpyxl import Workbook
 
 from app.image.image_controller import Image
 from app.image.images_list import ImagesList
-from app.main.table_controller import TableView
+from app.main.table_controller import TableController
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi("view/window.ui", self)
-        self.setup_connections()
         self.images_list = ImagesList()
+        self.setup_connections()
 
-        # Create and add the TableView instance
-        self.table_view = TableView(self)
-        self.verticalLayout.addWidget(self.table_view)
+        self.linesTable = self.findChild(QtWidgets.QTableWidget, "linesTable")
+        self.table_controller = TableController(self.linesTable)
 
     def setup_connections(self):
-        self.pushButton_ImageFolder.clicked.connect(self.load_images_from_folder)
+        self.pushButton_ImageFolder.pressed.connect(self.load_images_from_folder)
 
     def load_images_from_folder(self):
         folder_path = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select images folder')
@@ -31,10 +30,15 @@ class MainWindow(QtWidgets.QMainWindow):
             for root, dirs, files in os.walk(folder_path):
                 image_id = 0
                 for file_name in files:
-                    if file_name.lower().endswith(('.jpg', '.tiff', '.png')):
+                    if file_name.lower().endswith(('.jpg', '.tiff', '.png', '.tif', '.jpeg')):
                         image_object = Image(image_id, root, file_name)
-                        image_object.load_image()
-                        self.images_list.add_image_to_list(image_object)
-                        image_id += 1
+                        try:
+                            image_object.load_image()
+                            self.images_list.add_image_to_list(image_object)
+                            image_id += 1
+                        except PermissionError as e:
+                            print(f"PermissionError: {e}")
+                        except Exception as e:
+                            print(f"An unexpected error occurred: {e}")
 
-            self.table_view.update_with_image(self.images_list.i
+            self.table_controller.update_with_image(self.images_list.images_list)
--- app/main/table_controller.py
+++ app/main/table_controller.py
@@ -1,29 +1,28 @@
 from PyQt6 import QtWidgets, uic
+from PyQt6.QtWidgets import QTableWidget, QTableWidgetItem
 from PyQt6.QtCore import Qt
 
 
-class TableView(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        super(TableView, self).__init__(parent)
-        uic.loadUi("view/table.ui", self)
-        self.tableWidget = self.findChild(QtWidgets.QTableWidget, "tableWidget")
+class TableController(QtWidgets.QWidget):
+    def __init__(self, table_widget: QTableWidget):
+        self.table_widget = table_widget
         self.configure_table()
 
     def configure_table(self):
-        self.tableWidget.setColumnCount(9)
-        self.tableWidget.setHorizontalHeaderLabels(["Selected", "Processed", "Name", "N of Lines", "Average pitch", "Average CD",
-                                                    "CD std", "Unbiased LWR", "Unbiased LWR fit"])
+        self.table_widget.setColumnCount(9)
+        self.table_widget.setHorizontalHeaderLabels(["Selected", "Processed", "Name", "N of Lines", "Average pitch",
+                                                    "Average CD", "CD std", "Unbiased LWR", "Unbiased LWR fit"])
 
     def update_with_image(self, images_list):
-        self.tableWidget.setRowCount(len(images_list) + 2)
+        self.table_widget.setRowCount(len(images_list) + 2)
         for idx, image in enumerate(images_list):
-            check_box = QtWidgets.QTableWidgetItem()
+            check_box = QTableWidgetItem()
             check_box.setFlags(Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEnabled)
             check_box.setCheckState(Qt.CheckState.Unchecked if not image.selected else Qt.CheckState.Checked)
 
-            self.tableWidget.setItem(idx, 0, check_box)
-            self.tableWidget.setItem(idx, 1, QtWidgets.QTableWidgetItem(image.file_name))
-            self.tableWidget.setItem(idx, 2, QtWidgets.QTableWidgetItem('Yes' if image.processed else 'No'))
+            self.table_widget.setItem(idx, 0, check_box)
+            self.table_widget.setItem(idx, 1, QTableWidgetItem('Yes' if image.processed else 'No'))
+            self.table_widget.setItem(idx, 2, QTableWidgetItem(image.file_name))
 
 
 
