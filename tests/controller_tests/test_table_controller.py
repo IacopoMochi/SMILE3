a6a05800b573b1b861f814b17cafd4a4da920ceb
Emilia <emiliaburdak@gmail.com>
2024-07-24 10:54:14 +0200

    fix: update test_add_average_image test

--- tests/controller_tests/test_table_controller.py
+++ tests/controller_tests/test_table_controller.py
@@ -1,5 +1,5 @@
 import unittest
-from PyQt6.QtWidgets import QApplication, QTableWidget
+from PyQt6.QtWidgets import QApplication, QTableWidget, QTableWidgetItem
 from PyQt6.QtCore import Qt
 from app.models.image_container import Image
 from app.models.images_list import ImagesList
@@ -33,6 +33,15 @@ class TestTableController(unittest.TestCase):
         self.images_list = ImagesList()
         self.images_list.images_list = [self.image1, self.image2]
 
+        self.table_widget.setRowCount(len(self.images_list.images_list))
+        for img in self.images_list.images_list:
+            check_item = QTableWidgetItem()
+            check_item.setCheckState(Qt.CheckState.Checked)
+            self.table_widget.setItem(img.id, 0, check_item)
+            for col in range(1, 3):
+                self.table_widget.setItem(img.id, col, QTableWidgetItem(""))
+            img.processed = True
+
     def tearDown(self):
         self.table_widget = None
         self.table_controller = None
@@ -56,7 +65,7 @@ class TestTableController(unittest.TestCase):
             self.assertTrue(check_box.flags() & Qt.ItemFlag.ItemIsUserCheckable)
             expected_check_state = Qt.CheckState.Checked if image.selected else Qt.CheckState.Unchecked
             self.assertEqual(check_box.checkState(), expected_check_state)
-            self.assertEqual(self.table_widget.item(idx, 1).text(), 'No')
+            self.assertEqual(self.table_widget.item(idx, 1).text(), 'Yes')
             self.assertEqual(self.table_widget.item(idx, 2).text(), image.file_name.split('.')[0])
 
     def test_mark_image_as_processed(self):
@@ -74,7 +83,7 @@ class TestTableController(unittest.TestCase):
         self.assertEqual(self.table_widget.item(0, 6).text(), "0.45000")
 
     def test_add_average_image(self):
-        average_image = AverageImage(self.images_list)
+        average_image = AverageImage(self.images_list, self.table_widget)
 
         row_count_needed = average_image.image.id + 1
         if self.table_widget.rowCount() < row_count_needed:
