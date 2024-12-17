48ac6865335c748dee956d1b53df4f4c82dd842d
Iacopo Mochi <mochi.iacopo@gmail.com>
2024-12-18 00:05:20 +0100

    improvements to the contact simulator

--- app/view/contacts_simulator.py
+++ app/view/contacts_simulator.py
@@ -10,9 +10,7 @@ def contact_image(CD: float, pitch: float, arrangement: float, pixel_size: float
 
     centers = []
 
-    origin = 0
     edge = CD
-    end = 1
 
     origin_value = spline_values[0]
     edge_value = spline_values[2]
@@ -26,13 +24,12 @@ def contact_image(CD: float, pitch: float, arrangement: float, pixel_size: float
         for cx in np.arange(-image_width*pixel_size/2, image_width*pixel_size/2, pitch):
             for cy in np.arange(-image_height * pixel_size / 2, image_height * pixel_size / 2, pitch):
 
-                radius = edge+np.random.random(1)*radius_error
-                inner_edge = radius[0] * inner
-                outer_edge = radius[0]* outer
-                taper = outer_edge + (1 - outer_edge) * taper
-
+                radius = edge / pitch + np.random.random(1) * radius_error
+                x_inner_edge = radius[0] * inner
+                x_outer_edge = radius[0] * outer
+                x_taper = x_outer_edge + (1 - x_outer_edge) * taper
                 radii.append(radius)
-                xs = np.array([origin, inner_edge, radius, outer_edge, taper, end])
+                xs = np.array([0, x_inner_edge, radius[0], x_outer_edge, x_taper, np.sqrt(2)])
                 ys = np.array([origin_value, inner_edge_value, edge_value, outer_edge_value, taper_value, end_value])
                 spline = interpolate.PchipInterpolator(xs, ys)
 
@@ -49,17 +46,16 @@ def contact_image(CD: float, pitch: float, arrangement: float, pixel_size: float
 
     elif arrangement == "triangular":
         cnt = 0
-        for cx in np.arange(-image_width*pixel_size/2, image_width*pixel_size/2, pitch):
+        for cx in np.arange(-image_width*pixel_size/2+CD, image_width*pixel_size/2-CD, pitch):
             cnt += 1
-            for cy in np.arange(-image_height * pixel_size / 2, image_height * pixel_size / 2, pitch):
+            for cy in np.arange(-image_height * pixel_size / 2+CD, image_height * pixel_size / 2-CD, pitch):
 
-                radius = edge + np.random.random(1) * radius_error
-                xinner_edge = radius[0] * inner
-                xouter_edge = radius[0] * outer
-                xtaper = xouter_edge + (1 - xouter_edge) * taper
+                radius = edge/pitch + np.random.random(1) * radius_error
+                x_inner_edge = radius[0] * inner
+                x_outer_edge = radius[0] * outer
+                x_taper = x_outer_edge + (1 - x_outer_edge) * taper
                 radii.append(radius)
-                xs = np.array([0, xinner_edge, radius[0], xouter_edge, xtaper, 1])
-                print(xs)
+                xs = np.array([0, x_inner_edge, radius[0], x_outer_edge, x_taper, 1])
                 ys = np.array([origin_value, inner_edge_value, edge_value, outer_edge_value, taper_value, end_value])
                 spline = interpolate.PchipInterpolator(xs, ys)
 
@@ -67,20 +63,24 @@ def contact_image(CD: float, pitch: float, arrangement: float, pixel_size: float
                     centers.append((cx, cy+pitch/2))
                 else:
                     centers.append((cx, cy))
-                    center = centers[-1]
-                    local_map = np.logical_and((np.abs(x - center[0]) <= pitch / 2), (np.abs(y - center[1]) <= pitch / 2))
-                    rx = x[local_map] - cx
-                    ry = y[local_map] - cy
-                    maximum = np.max([rx, ry])
-                    rx = rx / maximum
-                    ry = ry / maximum
-                    rho = np.sqrt(rx ** 2 + ry ** 2)
-                    rho_noise = np.random.poisson(rho * noise) / noise
-                    image[local_map] = spline(rho_noise)
+                center = centers[-1]
+                local_map = np.logical_and((np.abs(x - center[0]) <= pitch / 2), (np.abs(y - center[1]) <= pitch / 2))
+                rx = x[local_map] - center[0]
+                ry = y[local_map] - center[1]
+                maximum = np.max([rx, ry])
+                rx = rx / maximum
+                ry = ry / maximum
+                rho = np.sqrt(rx ** 2 + ry ** 2)
+                rho_noise = np.random.poisson(rho * noise) / noise
+                image[local_map] = spline(rho_noise)
 
 
+    image[image<0]=0
     return image, centers, radii
 
-image, centers, radii = contact_image(16, 32, 'triangular', 0.2, 2000, 1200, 5, 0.2, 0.2, 0.1, np.array([0.7,0.9,1.0,0.9,0.1,1.0]), 1)
-plt.imshow(image)
+image, centers, radii = contact_image(32, 64, 'triangular', 0.8, 1000, 600, 100, 0.95, 1.05, 0.1, np.array([0.7,0.95,1.0,0.95,0.1,0]), 0.1)
+fig, ax = plt.subplots()
+cs = ax.imshow(image)
+contours = ax.plot(centers[10])
+fig.colorbar(cs)
 plt.show()
