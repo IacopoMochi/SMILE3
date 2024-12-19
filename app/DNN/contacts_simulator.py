546df7f6aeb474af45f5ec17991c2c60278656ac
Iacopo Mochi <mochi.iacopo@gmail.com>
2024-12-20 00:22:10 +0100

    Network training

--- app/DNN/contacts_simulator.py
+++ app/DNN/contacts_simulator.py
@@ -1,23 +1,27 @@
+import random
+
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy import interpolate
+from PIL import Image
 
-def contact_image(CD: float, pitch: float, arrangement: float, pixel_size: float, image_width: float, image_height: float, noise: float, inner: float, outer: float, taper: float, spline_values: np.ndarray, radius_error: float):
+def contact_image(CD: float, pitch: float, arrangement: str, pixel_size: float, image_width: float, image_height: float, noise: float, inner: float, outer: float, taper: float, spline_values: np.ndarray, radius_error: float):
 
     x, y = np.meshgrid(np.linspace(-image_width*pixel_size/2, image_width*pixel_size/2, image_width),
                        np.linspace(-image_height*pixel_size/2, image_height*pixel_size/2, image_height))
-    image = np.zeros(np.shape(x))
 
     centers = []
 
     edge = CD
 
     origin_value = spline_values[0]
-    edge_value = spline_values[2]
     inner_edge_value = spline_values[1]
+    edge_value = spline_values[2]
     outer_edge_value = spline_values[3]
     taper_value = spline_values[4]
     end_value = spline_values[5]
+
+    image = np.zeros(np.shape(x)) + end_value
 
     radii = []
     if arrangement == "orthogonal":
@@ -28,7 +32,7 @@ def contact_image(CD: float, pitch: float, arrangement: float, pixel_size: float
                 x_inner_edge = radius[0] * inner
                 x_outer_edge = radius[0] * outer
                 x_taper = x_outer_edge + (1 - x_outer_edge) * taper
-                radii.append(radius)
+                #radii.append(radius*pitch)
                 xs = np.array([0, x_inner_edge, radius[0], x_outer_edge, x_taper, np.sqrt(2)])
                 ys = np.array([origin_value, inner_edge_value, edge_value, outer_edge_value, taper_value, end_value])
                 spline = interpolate.PchipInterpolator(xs, ys)
@@ -43,6 +47,10 @@ def contact_image(CD: float, pitch: float, arrangement: float, pixel_size: float
                 rho = np.sqrt(rx**2 + ry**2)
                 rho_noise = np.random.poisson(rho*noise)/noise
                 image[local_map] = spline(rho_noise)
+                x_profile = np.linspace(0, np.sqrt(2),100)
+                profile = spline(x_profile)
+                radius_position = np.argmin(np.diff(profile)) * pitch/100
+                radii.append(radius_position)
 
     elif arrangement == "triangular":
         cnt = 0
@@ -54,7 +62,7 @@ def contact_image(CD: float, pitch: float, arrangement: float, pixel_size: float
                 x_inner_edge = radius[0] * inner
                 x_outer_edge = radius[0] * outer
                 x_taper = x_outer_edge + (1 - x_outer_edge) * taper
-                radii.append(radius)
+                #radii.append(radius*pitch)
                 xs = np.array([0, x_inner_edge, radius[0], x_outer_edge, x_taper, 1])
                 ys = np.array([origin_value, inner_edge_value, edge_value, outer_edge_value, taper_value, end_value])
                 spline = interpolate.PchipInterpolator(xs, ys)
@@ -73,14 +81,57 @@ def contact_image(CD: float, pitch: float, arrangement: float, pixel_size: float
                 rho = np.sqrt(rx ** 2 + ry ** 2)
                 rho_noise = np.random.poisson(rho * noise) / noise
                 image[local_map] = spline(rho_noise)
+                x_profile = np.linspace(0, np.sqrt(2), 100)
+                profile = spline(x_profile)
+                radius_position = np.argmin(np.diff(profile)) * pitch / 100
+                radii.append(radius_position)
 
-
-    image[image<0]=0
+    image[image < 0] = 0
     return image, centers, radii
 
-image, centers, radii = contact_image(32, 64, 'triangular', 0.8, 1000, 600, 100, 0.95, 1.05, 0.1, np.array([0.7,0.95,1.0,0.95,0.1,0]), 0.1)
-fig, ax = plt.subplots()
-cs = ax.imshow(image)
-contours = ax.plot(centers[10])
-fig.colorbar(cs)
-plt.show()
+
+for img_idx in range(0, 100):
+    CD = random.randint(20,100)
+    pitch = CD * random.randint(6,10)/4
+    arrangement_flag = random.randint(0,1)
+    if arrangement_flag == 0:
+        arrangement = "orthogonal"
+    else:
+        arrangement = "triangular"
+    image_height = random.randint(600,1000)
+    image_width = random.randint(800, 2000)
+    noise_idx = random.randint(0,9)
+    noise = np.round(np.linspace(100,10000, 10))
+    noise_value = noise[noise_idx]
+    inner = random.randint(1,10) / 100
+    outer = random.randint(1, 10) / 100
+    taper = random.randint(1, 20) / 100
+
+    center_value = random.randint(2,10)/10
+    inner_value = random.randint(2, 10) / 10
+    outer_value = inner_value
+    edge_value = random.randint(2, 10) / 10
+    taper_value = random.randint(6, 7) / 10
+    background_value = random.randint(0, 2) / 10
+    radius_error = random.randint(0,15)/100
+    image, centers, radii = contact_image(32, 64, 'triangular', 0.8, 1000,
+                                      600, 100, 0.05, 1.05, 0.1,
+                                      np.array([center_value, inner_value, edge_value, outer_value,
+                                                taper_value,background_value]), 0.15)
+
+    im = Image.fromarray(np.uint8(image*255))
+    s = "synt_circles_"+f'{img_idx:03}'+".jpg"
+    print(s)
+    im.save(s)
+
+# fig, ax = plt.subplots(1,1)
+# cs = ax.imshow(image)
+# #
+# t = np.linspace(0, 2*np.pi,100)
+# for n in range(0, np.size(radii)):
+#     c = centers[n]
+#     r = radii[n]
+#     ax.plot(c[0]/0.8+500, c[1]/0.8+300,'ro')
+#     ax.plot(c[0]/0.8+500 + r*np.cos(t), c[1]/0.8+300 + r*np.sin(t),'-b')
+# fig.colorbar(cs)
+# plt.show()
