8218e904219bd0760ac6436d4c733826f8459c46
Iacopo Mochi <mochi.iacopo@gmail.com>
2024-12-20 08:36:25 +0100

    Network work

--- app/DNN/contacts_simulator.py
+++ app/DNN/contacts_simulator.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 from scipy import interpolate
 from PIL import Image
 
-def contact_image(CD: float, pitch: float, arrangement: str, pixel_size: float, image_width: float, image_height: float, noise: float, inner: float, outer: float, taper: float, spline_values: np.ndarray, radius_error: float):
+def contact_image(CD: float, pitch: float, arrangement: str, pixel_size: float, image_width: float, image_height: float, noise: float, inner: float, outer: float, taper: float, spline_values: np.ndarray, radius_error: float, missc: int):
 
     x, y = np.meshgrid(np.linspace(-image_width*pixel_size/2, image_width*pixel_size/2, image_width),
                        np.linspace(-image_height*pixel_size/2, image_height*pixel_size/2, image_height))
@@ -21,70 +21,73 @@ def contact_image(CD: float, pitch: float, arrangement: str, pixel_size: float, 
     taper_value = spline_values[4]
     end_value = spline_values[5]
 
+
     image = np.zeros(np.shape(x)) + end_value
 
     radii = []
     if arrangement == "orthogonal":
         for cx in np.arange(-image_width*pixel_size/2, image_width*pixel_size/2, pitch):
             for cy in np.arange(-image_height * pixel_size / 2, image_height * pixel_size / 2, pitch):
+                miss = random.randint(1, missc) - 2
+                if miss>=0:
+                    radius = edge / pitch + np.random.random(1) * radius_error
+                    x_inner_edge = radius[0] * inner
+                    x_outer_edge = radius[0] * outer
+                    x_taper = x_outer_edge + (1 - x_outer_edge) * taper
+                    #radii.append(radius*pitch)
+                    xs = np.array([0, x_inner_edge, radius[0], x_outer_edge, x_taper, np.sqrt(2)])
+                    ys = np.array([origin_value, inner_edge_value, edge_value, outer_edge_value, taper_value, end_value])
+                    spline = interpolate.PchipInterpolator(xs, ys)
 
-                radius = edge / pitch + np.random.random(1) * radius_error
-                x_inner_edge = radius[0] * inner
-                x_outer_edge = radius[0] * outer
-                x_taper = x_outer_edge + (1 - x_outer_edge) * taper
-                #radii.append(radius*pitch)
-                xs = np.array([0, x_inner_edge, radius[0], x_outer_edge, x_taper, np.sqrt(2)])
-                ys = np.array([origin_value, inner_edge_value, edge_value, outer_edge_value, taper_value, end_value])
-                spline = interpolate.PchipInterpolator(xs, ys)
-
-                centers.append((cx, cy))
-                local_map = np.logical_and((np.abs(x-cx) <= pitch/2), (np.abs(y-cy) <= pitch/2))
-                rx = x[local_map]-cx
-                ry = y[local_map]-cy
-                maximum = np.max([rx, ry])
-                rx = rx / maximum
-                ry = ry / maximum
-                rho = np.sqrt(rx**2 + ry**2)
-                rho_noise = np.random.poisson(rho*noise)/noise
-                image[local_map] = spline(rho_noise)
-                x_profile = np.linspace(0, np.sqrt(2),100)
-                profile = spline(x_profile)
-                radius_position = np.argmin(np.diff(profile)) * pitch/100
-                radii.append(radius_position)
+                    centers.append((cx, cy))
+                    local_map = np.logical_and((np.abs(x-cx) <= pitch/2), (np.abs(y-cy) <= pitch/2))
+                    rx = x[local_map]-cx
+                    ry = y[local_map]-cy
+                    maximum = np.max([rx, ry])
+                    rx = rx / maximum
+                    ry = ry / maximum
+                    rho = np.sqrt(rx**2 + ry**2)
+                    rho_noise = np.random.poisson(rho*noise)/noise
+                    image[local_map] = spline(rho_noise)
+                    x_profile = np.linspace(0, np.sqrt(2),100)
+                    profile = spline(x_profile)
+                    radius_position = np.argmin(np.diff(profile)) * pitch/100
+                    radii.append(radius_position)
 
     elif arrangement == "triangular":
         cnt = 0
         for cx in np.arange(-image_width*pixel_size/2+CD, image_width*pixel_size/2-CD, pitch):
             cnt += 1
             for cy in np.arange(-image_height * pixel_size / 2+CD, image_height * pixel_size / 2-CD, pitch):
+                miss = random.randint(1, missc) - 2
+                if miss >= 0:
+                    radius = edge/pitch + (np.random.random(1)-0.5) * radius_error
+                    x_inner_edge = radius[0] * inner
+                    x_outer_edge = radius[0] * outer
+                    x_taper = x_outer_edge + (1 - x_outer_edge) * taper
+                    #radii.append(radius*pitch)
+                    xs = np.array([0, x_inner_edge, radius[0], x_outer_edge, x_taper, np.sqrt(2)])
+                    ys = np.array([origin_value, inner_edge_value, edge_value, outer_edge_value, taper_value, end_value])
+                    spline = interpolate.PchipInterpolator(xs, ys)
 
-                radius = edge/pitch + np.random.random(1) * radius_error
-                x_inner_edge = radius[0] * inner
-                x_outer_edge = radius[0] * outer
-                x_taper = x_outer_edge + (1 - x_outer_edge) * taper
-                #radii.append(radius*pitch)
-                xs = np.array([0, x_inner_edge, radius[0], x_outer_edge, x_taper, 1])
-                ys = np.array([origin_value, inner_edge_value, edge_value, outer_edge_value, taper_value, end_value])
-                spline = interpolate.PchipInterpolator(xs, ys)
-
-                if np.mod(cnt, 2) == 0:
-                    centers.append((cx, cy+pitch/2))
-                else:
-                    centers.append((cx, cy))
-                center = centers[-1]
-                local_map = np.logical_and((np.abs(x - center[0]) <= pitch / 2), (np.abs(y - center[1]) <= pitch / 2))
-                rx = x[local_map] - center[0]
-                ry = y[local_map] - center[1]
-                maximum = np.max([rx, ry])
-                rx = rx / maximum
-                ry = ry / maximum
-                rho = np.sqrt(rx ** 2 + ry ** 2)
-                rho_noise = np.random.poisson(rho * noise) / noise
-                image[local_map] = spline(rho_noise)
-                x_profile = np.linspace(0, np.sqrt(2), 100)
-                profile = spline(x_profile)
-                radius_position = np.argmin(np.diff(profile)) * pitch / 100
-                radii.append(radius_position)
+                    if np.mod(cnt, 2) == 0:
+                        centers.append((cx, cy+pitch/2))
+                    else:
+                        centers.append((cx, cy))
+                    center = centers[-1]
+                    local_map = np.logical_and((np.abs(x - center[0]) <= pitch / 2), (np.abs(y - center[1]) <= pitch / 2))
+                    rx = x[local_map] - center[0]
+                    ry = y[local_map] - center[1]
+                    maximum = np.max([rx, ry])
+                    rx = rx / maximum
+                    ry = ry / maximum
+                    rho = np.sqrt(rx ** 2 + ry ** 2)
+                    rho_noise = np.random.poisson(rho * noise) / noise
+                    image[local_map] = spline(rho_noise)
+                    x_profile = np.linspace(0, np.sqrt(2), 100)
+                    profile = spline(x_profile)
+                    radius_position = np.argmin(np.diff(profile)) * pitch / 100
+                    radii.append(radius_position)
 
     image[image < 0] = 0
     return image, centers, radii
@@ -103,23 +106,28 @@ for img_idx in range(0, 100):
     noise_idx = random.randint(0,9)
     noise = np.round(np.linspace(100,10000, 10))
     noise_value = noise[noise_idx]
-    inner = random.randint(1,10) / 100
-    outer = random.randint(1, 10) / 100
-    taper = random.randint(1, 20) / 100
+    inner = 1-random.randint(1,6) / 100
+    outer = 1 + random.randint(1, 6) / 100
+    taper = random.randint(1, 2) / 10
 
-    center_value = random.randint(2,10)/10
-    inner_value = random.randint(2, 10) / 10
+    center_value = random.randint(3,10)/10
+    inner_value = random.randint(5, 10) / 10
     outer_value = inner_value
-    edge_value = random.randint(2, 10) / 10
-    taper_value = random.randint(6, 7) / 10
-    background_value = random.randint(0, 2) / 10
-    radius_error = random.randint(0,15)/100
-    image, centers, radii = contact_image(32, 64, 'triangular', 0.8, 1000,
-                                      600, 100, 0.05, 1.05, 0.1,
-                                      np.array([center_value, inner_value, edge_value, outer_value,
-                                                taper_value,background_value]), 0.15)
+    edge_value = random.randint(5, 10) / 10
+    taper_value = random.randint(1, 5) / 10
+    background_value = random.randint(0, round(10*taper_value)) / 10
+    radius_error = random.randint(0,20)/100
+    missc = random.randint(10,100)
+    # image, centers, radii = contact_image(32, 64, 'triangular', 0.8, 1000,
+    #                                       600, 100, 0.05, 1.05, 0.1,
+    #                                       np.array([center_value, inner_value, edge_value, outer_value,
+    #                                                 taper_value, background_value]), 0.15, 100)
+    image, centers, radii = contact_image(CD, pitch, arrangement, 0.8, image_width,
+                                          image_height, noise_value, inner, outer, taper,
+                                          np.array([center_value, inner_value, edge_value, outer_value,
+                                                    taper_value, background_value]), radius_error, missc)
 
-    im = Image.fromarray(np.uint8(image*255))
+    im = Image.fromarray(np.uint8(image*255/np.max(image)))
     s = "synt_circles_"+f'{img_idx:03}'+".jpg"
     print(s)
     im.save(s)
