8cda6cac7d4161c0f727cf12f6b0defdedc07240
Iacopo Mochi <mochi.iacopo@gmail.com>
2025-02-04 07:51:02 +0100

    Update image_loader.py

--- app/processors/image_loader.py
+++ app/processors/image_loader.py
@@ -1,5 +1,6 @@
 import os
 from PIL import Image
+from PIL.ExifTags import TAGS
 
 import numpy as np
 
@@ -28,4 +29,28 @@ class ImageLoader:
         file_path = os.path.join(self.folder, self.file_name)
         with Image.open(file_path) as img:
             img = np.rot90(img, 3)
+
+            info_dict = {
+                "Filename": img.filename,
+                "Image Size": img.size,
+                "Image Height": img.height,
+                "Image Width": img.width,
+                "Image Format": img.format,
+                "Image Mode": img.mode,
+                "Image is Animated": getattr(img, "is_animated", False),
+                "Frames in Image": getattr(img, "n_frames", 1)
+            }
+
+            for label, value in info_dict.items():
+                print(f"{label:25}: {value}")
+
+            metadata = img.getexif()
+            for tag_id in metadata:
+                # get the tag name, instead of human unreadable tag id
+                tag = TAGS.get(tag_id, tag_id)
+                data = exifdata.get(tag_id)
+                # decode bytes
+                if isinstance(data, bytes):
+                    data = data.decode()
+                print(f"{tag:25}: {data}")
             return img
