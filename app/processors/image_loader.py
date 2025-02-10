9a860f4a0c0af8893b18605d8a255e493e54d1c5
Iacopo Mochi <mochi.iacopo@gmail.com>
2025-02-10 16:56:05 +0100

    Pixel size reading
    Modified the image loader processor to include the extraction of the pixel size from the image header

--- app/processors/image_loader.py
+++ app/processors/image_loader.py
@@ -1,5 +1,6 @@
 import os
 from PIL import Image
+from PIL.ExifTags import TAGS
 
 import numpy as np
 
@@ -27,5 +28,55 @@ class ImageLoader:
 
         file_path = os.path.join(self.folder, self.file_name)
         with Image.open(file_path) as img:
+            info_dict = {
+                "Filename": img.filename,
+                "Image Size": img.size,
+                "Image Height": img.height,
+                "Image Width": img.width,
+                "Image Format": img.format,
+                "Image Mode": img.mode,
+                "Image is Animated": getattr(img, "is_animated", False),
+                "Frames in Image": getattr(img, "n_frames", 1)
+            }
+            # for label, value in info_dict.items():
+            #     print(f"{label:25}: {value}")
+
+            metadata = img.getexif()
+            for tag_id in metadata:
+                # get the tag name, instead of human unreadable tag id
+                tag = TAGS.get(tag_id, tag_id)
+                data = metadata.get(tag_id)
+                # decode bytes
+                if isinstance(data, bytes):
+                    data = data.decode()
+                S = (f"{data}")
+                idx = S.find("Image Pixel Size = ")
+                if idx > 0:
+                    SC = S[idx:idx+35]
+                    end_of_line = (SC.find('\n'))
+                    equal = (SC.find('='))
+                    S = S[idx:idx + end_of_line - 1]
+                    if S[-2:] == "pm":
+                        scaling_factor = 0.001
+                    elif S[-2:] == "nm":
+                        scaling_factor = 1
+                    pixel_size = np.double(SC[equal+2: end_of_line-3])
+                    print(pixel_size*scaling_factor)
+
+            for tag, value in img.tag_v2.items():
+                stringa = ""
+                if tag == 40092:
+                    for byte in value:
+                        if byte > 0:
+                            stringa = stringa + chr(byte)
+                    index_PixelSize = stringa.find("PixelSize")
+                    stringa = stringa[index_PixelSize+10:-1]
+                    index_return = stringa.find("\n")
+                    stringa=stringa[0:index_return-1]
+                    print(stringa)
+
+
+
+
             img = np.rot90(img, 3)
             return img
